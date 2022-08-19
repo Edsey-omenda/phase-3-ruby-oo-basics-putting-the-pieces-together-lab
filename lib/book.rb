51a8a3f2c7a948275b64a627d85465437a5d5881
Edsey omenda <omendajames0317@gmail.com>
2022-08-19 17:25:49 +0300

    commit tree 1eaf2ce774d5b1da7453c2e7c9318072c4f40bab

--- lib/book.rb
+++ lib/book.rb
@@ -1,15 +1,15 @@
 class Book
-    attr_reader :title
-    attr_writer :title
-    attr_reader :author
-    attr_writer :author
-    attr_reader :page_count
-    attr_writer :page_count
-    attr_reader :genre
-    attr_writer :genre
+    # attr_reader :title
+    # attr_writer :title
+    # attr_reader :author
+    # attr_writer :author
+    # attr_reader :page_count
+    # attr_writer :page_count
+    # attr_reader :genre
+    # attr_writer :genre
 
     # attr_reader :turn_page
-    attr_accessor :turn_page
+    attr_accessor :title, :author, :page_count, :genre, :turn_page
 
     def initialize(title)
         @title = title
