1c396babe6e12fd9e6da4e774735092114b45a05
Edsey omenda <omendajames0317@gmail.com>
2022-08-19 17:00:21 +0300

    completed

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,26 @@
+class Book
+    attr_reader :title
+    attr_writer :title
+    attr_reader :author
+    attr_writer :author
+    attr_reader :page_count
+    attr_writer :page_count
+    attr_reader :genre
+    attr_writer :genre
 
+    # attr_reader :turn_page
+    attr_accessor :turn_page
+
+    def initialize(title)
+        @title = title
+        # @author_name = author_name
+        # @page_count = page_count
+        # @genre = genre
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!" 
+    end
+
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,29 @@
 # Make your shoe class here!
+class Shoe
+    attr_reader :brand
+    attr_writer :brand
+    attr_reader :color
+    attr_writer :color
+    attr_reader :size
+    attr_writer :size
+    attr_reader :material
+    attr_writer :material
+    attr_reader :condition
+    attr_writer :condition
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    # attr_accessor :cobble
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+        
+    end
+
+
+end
+
+
